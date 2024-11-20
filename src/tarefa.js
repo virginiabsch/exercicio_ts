3164b778134c2481a13eec2e2ceb8ce8cb477ada
virginiabsch <virginiabsch@gmail.com>
2024-11-20 14:50:56 -0300

    initial commit

---
+++ src/tarefa.js
@@ -0,0 +1,13 @@
+"use strict";
+//função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log(multiplicar(2, 4));
+//R=8
+//função de saudação
+function dizOi(nome) {
+    return "Olá " + nome;
+}
+console.log(dizOi("Carlos"));
+//R= Olá Carlos
---
+++ src/tarefa.ts
@@ -0,0 +1,14 @@
+//função de multiplicação
+function multiplicar(a: number, b: number): number {
+     return a * b;
+}
+console.log(multiplicar(2, 4));
+//R=8
+
+
+//função de saudação
+function dizOi(nome: string): string {
+    return "Olá " + nome;
+}
+console.log(dizOi("Carlos"));
+//R= Olá Carlos
